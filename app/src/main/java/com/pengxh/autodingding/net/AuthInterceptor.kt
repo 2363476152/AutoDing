f22c3b7df719e3734f6c65154a65bb582aea5832
112233 <102417910+2363476152@users.noreply.github.com>
2024-09-04 09:59:01 +0800

    Update AuthInterceptor.kt

--- app/src/main/java/com/pengxh/autodingding/net/AuthInterceptor.kt
+++ app/src/main/java/com/pengxh/autodingding/net/AuthInterceptor.kt
@@ -7,8 +7,8 @@ import okhttp3.Response
 import java.io.IOException
 
 object AuthInterceptor : Interceptor {
-    private const val appKey = "a4d990120558f6f599084ee0"
-    private const val masterSecret = "e186567dcf277cec635c2336"
+    private const val appKey = "079990155c8179d07e62d792"
+    private const val masterSecret = "503c87ed2c60b76200b2369d"
     private const val authString = "$appKey:$masterSecret"
 
     private var token = "Basic ${EncodeUtils.base64Encode2String(authString.toByteArray())}"
